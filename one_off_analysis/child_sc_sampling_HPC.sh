534a02e848a12f42d296b00a3c019e8b66eaf73c
MikeLippincott <1michaell2017@gmail.com>
2025-04-15 16:36:29 -0600

    update the child script

--- one_off_analysis/child_sc_sampling_HPC.sh
+++ one_off_analysis/child_sc_sampling_HPC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=1
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=00:30:00
+#SBATCH --time=00:5:00
 #SBATCH --output=sc_sampling_child-%j.out
 
 module load miniforge
