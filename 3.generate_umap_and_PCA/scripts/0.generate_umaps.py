ec031fcac68bccd4c5daeedb6f73690063930aed
MikeLippincott <1michaell2017@gmail.com>
2026-07-09 11:48:17 -0600

    adding py scripts

--- 3.generate_umap_and_PCA/scripts/0.generate_umaps.py
+++ 3.generate_umap_and_PCA/scripts/0.generate_umaps.py
@@ -9,7 +9,7 @@ import pathlib
 import pandas as pd
 import umap
 
-# In[ ]:
+# In[2]:
 
 
 def fit_umap_to_the_first_timepoint(
@@ -65,6 +65,55 @@ def fit_umap_to_the_first_timepoint(
 
     # apply the model to the rest of the data
     umap_embeddings = umap_model.transform(df)
+    # create a dataframe with the umap fit and the metadata
+    umap_df = pd.DataFrame(umap_embeddings, columns=["UMAP_0", "UMAP_1"])
+    # add the metadata to the dataframe
+    umap_df = pd.concat([umap_df, metadata_df], axis=1)
+
+    return umap_df
+
+
+def fit_umap_to_all_timepoints(
+    df: pd.DataFrame,
+    metadata_columns: list,
+    feature_columns: list,
+    umap_model: umap.UMAP,
+) -> pd.DataFrame:
+    """
+    This function fits a UMAP model to all timepoints of the data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe containing all feature, metadata, and timepoint columns.
+    timepoint_column : str
+        The name of the column containing the timepoint information
+    metadata_columns : list
+        The names of the columns containing the metadata information
+    feature_columns : list
+        The names of the columns containing the feature information
+    umap_model : umap.UMAP
+        The UMAP model to use.
+
+    Returns
+    -------
+    pd.DataFrame
+        The UMAP embeddings for the data, with the metadata columns included.
+    """
+
+    df = df.copy()
+    metadata_df = df[metadata_columns]
+
+    # Prepare the data by dropping metadata columns, selecting feature columns, and removing rows with missing values
+    df = df.drop(metadata_columns, axis=1)
+    df = df[feature_columns]
+    df.dropna(axis=0, inplace=True)
+    metadata_df = metadata_df.loc[df.index]
+    df.reset_index(drop=True, inplace=True)
+    metadata_df.reset_index(drop=True, inplace=True)
+
+    # fit the model to all timepoints
+    umap_embeddings = umap_model.fit_transform(df)
     # create a dataframe with the umap fit and the metadata
     umap_df = pd.DataFrame(umap_embeddings, columns=["UMAP_0", "UMAP_1"])
     # add the metadata to the dataframe
@@ -137,3 +186,16 @@ for profile_level in dictionary_of_feature_sets.keys():
         umap_save_path.parent.mkdir(parents=True, exist_ok=True)
         # save the umap data
         umap_df.to_parquet(umap_save_path, index=False)
+        all_timepoints_umap_df = fit_umap_to_all_timepoints(
+            profile_df,
+            metadata_columns=metadata_columns,
+            feature_columns=feature_columns,
+            umap_model=umap_model,
+        )
+        # set the save path of the umap data for all timepoints
+        all_timepoints_umap_save_path = pathlib.Path(
+            f"../results/UMAP/{profile_level}_{profile}_all_timepoints_umap.parquet"
+        ).resolve()
+        all_timepoints_umap_save_path.parent.mkdir(parents=True, exist_ok=True)
+        # save the umap data for all timepoints
+        all_timepoints_umap_df.to_parquet(all_timepoints_umap_save_path, index=False)
